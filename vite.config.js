4b0e1b68a02d621fb95cc91caccc7ff2d77d03e0
Pushpendra dwivedi <pushpendradwivedi9@gmail.com>
2026-05-22 15:59:15 +0530

    Done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
+  base: '/appzant/',
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, 'partials'),
